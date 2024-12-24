54659b16607b26fa802725dd1d42e2e8fcaf33a5
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-12-24 14:12:56 +0100

    Update pog.lua

--- pog.lua
+++ pog.lua
@@ -16,14 +16,3 @@ spawn(function()
         task.wait(600) -- Wait 10 minutes before claiming again
     end
 end)
-
-
-local Client = game:GetService('ReplicatedStorage').Library.Client
-local SaveMod = require(Client.Save).Get()
-local RankCmd = require(Client.RankCmds)
-local Network = require(Client.Network)
-
-for i = 1,84 do
-    local EggSlotInfo = {RankCmd.GetEggBundle(SaveMod["EggSlotsPurchased"] + 1)}
-    Network.InvokeServer("EggHatchSlotsMachine_RequestPurchase",EggSlotInfo[1]) task.wait(1)
-end
