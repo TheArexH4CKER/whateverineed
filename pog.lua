2d06d8edd0601520be4256536285c7cd15aad86c
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2024-12-28 18:33:46 +0100

    Update pog.lua

--- pog.lua
+++ pog.lua
@@ -16,3 +16,17 @@ spawn(function()
         task.wait(600) -- Wait 10 minutes before claiming again
     end
 end)
+
+
+task.wait(60)
+spawn(function()
+    while true do
+        pcall(function()   
+                local tier = 1
+                local type = "Lockpick A"
+                    game:GetService("ReplicatedStorage").Library.Client.Network["Lockpick Game: Request Unlock"]:Invoke(tier, type)
+        end)
+        task.wait(600) -- Wait 10 minutes before claiming again
+    end
+end)
+
